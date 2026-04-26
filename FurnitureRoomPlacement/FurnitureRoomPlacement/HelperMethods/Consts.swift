2f593992e4990285b92ea48964ecb7c7931ef5c5
Aarush Narang <aarushnarang@gmail.com>
2026-04-26 01:17:12 -0700

    update base url

--- FurnitureRoomPlacement/FurnitureRoomPlacement/HelperMethods/Consts.swift
+++ FurnitureRoomPlacement/FurnitureRoomPlacement/HelperMethods/Consts.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let baseURL = "https://d06e-2600-1012-b036-60ba-812f-ccd3-ca5c-e19b.ngrok-free.app/"
+let baseURL = "https://a088-2600-1012-b036-60ba-812f-ccd3-ca5c-e19b.ngrok-free.app"
 
 let furnitureAssets: [String: String] = [ // key: name, value: usdz file path
     "bedframe": "bedframe",
